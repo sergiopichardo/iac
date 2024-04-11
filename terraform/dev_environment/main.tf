a253d5926f60d0762470631c3b0e22397c68ac3c
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-04-10 22:13:47 -0500

    feat(vpc): add public subnet

--- terraform/dev_environment/main.tf
+++ terraform/dev_environment/main.tf
@@ -1,10 +1,21 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "dev_vpc" {
   cidr_block           = "10.123.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
 
   tags = {
-    Name = "dev"
+    Name = "dev-vpc"
   }
 }
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true  # instances launch into the subnet 
+                                  # should be assigned a public IP address
+  availability_zone       = "us-east-1a"
+
+  tags = {
+    Name = "dev-public-subnet"
+  }
+}
