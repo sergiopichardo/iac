7aa449d896bf40e3d745e69742fbe00208c4c8ca
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-04-15 19:08:49 -0500

    feat(dev_env): add security group with ingress and engress rules

--- terraform/dev_environment/main.tf
+++ terraform/dev_environment/main.tf
@@ -1,7 +1,9 @@
 resource "aws_vpc" "dev_vpc" {
   cidr_block           = "10.123.0.0/16"
+  assign_generated_ipv6_cidr_block = true
   enable_dns_hostnames = true
   enable_dns_support   = true
+
 
   tags = {
     Name = "dev-vpc"
@@ -36,9 +38,9 @@ resource "aws_route_table" "public_dev_rt" {
 }
 
 resource "aws_route" "default_route" {
-  route_table_id            = aws_route_table.public_dev_rt.id
-  destination_cidr_block    = "0.0.0.0/0"
-  gateway_id = aws_internet_gateway.dev_igw.id 
+  route_table_id         = aws_route_table.public_dev_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dev_igw.id
 }
 
 resource "aws_route_table_association" "dev_public_rt_association" {
@@ -46,40 +48,40 @@ resource "aws_route_table_association" "dev_public_rt_association" {
   route_table_id = aws_route_table.public_dev_rt.id
 }
 
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+resource "aws_security_group" "http_security_group" {
+  name        = "HTTP Security Group"
   description = "Allow TLS inbound traffic and all outbound traffic"
   vpc_id      = aws_vpc.dev_vpc.id
 
   tags = {
-    Name = "allow_tls"
+    Name = "HTTP Security Group"
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.http_security_group.id
   cidr_ipv4         = aws_vpc.dev_vpc.cidr_block
-  from_port         = 443
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 80
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv6" {
+  security_group_id = aws_security_group.http_security_group.id
   cidr_ipv6         = aws_vpc.dev_vpc.ipv6_cidr_block
-  from_port         = 443
+  from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 443
+  to_port           = 80
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.http_security_group.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
-  security_group_id = aws_security_group.allow_tls.id
+  security_group_id = aws_security_group.http_security_group.id
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
