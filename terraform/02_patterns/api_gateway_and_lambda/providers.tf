59fae002a65ab64eb972b3386b8cfb1cc052a6da
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2024-05-05 22:11:04 -0500

    feat(api_gateawy_and_lambda): add initial api gateway code

---
+++ terraform/02_patterns/api_gateway_and_lambda/providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 5.0"
+    }
+
+    random = {
+        source = "hashicorp/random"
+        version = "~> 3.6.1"
+    }
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+    shared_credentials_files = ["~/.aws/credentials"]
+    profile = "sergio"
+}
+
+provider "random" {}
